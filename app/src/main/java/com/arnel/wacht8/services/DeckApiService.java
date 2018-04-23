45e3a85cc3b9b8511ae08125393abd6b64ca3e44
Libbrecht Arne <arne.libbrecht@student.vives.be>
2018-04-23 15:43:52 +0200

    test

--- app/src/main/java/com/arnel/wacht8/services/DeckApiService.java
+++ app/src/main/java/com/arnel/wacht8/services/DeckApiService.java
@@ -25,7 +25,7 @@ public interface DeckApiService {
                     .addConverterFactory(GsonConverterFactory.create())
             .build();
 
-
+//test
     @GET("deck/new/shuffle")
     Call<Deck> getNewDeck();
 
